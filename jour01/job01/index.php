9b75653c2905497becdc69ad8e489a44d5e69d99
AslanUnravel <aslanhadef9@gmail.com>
2024-05-22 13:03:07 +0200

    bonjour

---
+++ jour01/job01/index.php
@@ -0,0 +1,8 @@
+<?php
+$str = 'laPlateforme';
+echo $str; 
+$str2 = " Vive la plateforme";
+echo $str2;
+$str3 = " !";
+echo $str3;
+?>
---
+++ jour01/job02/index.php
@@ -0,0 +1,8 @@
+<?php
+$val = 6;
+echo $val+=4;  
+$myBool = true;
+echo $myBool;
+$myBool = false;
+echo $myBool;
+?>
